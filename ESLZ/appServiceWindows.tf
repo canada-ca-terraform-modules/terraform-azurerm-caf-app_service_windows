501cd5ad55b9b609793584734cc701a2e8d76916
Maxime Mahdavian <maxime.mahdavian@ssc-spc.gc.ca>
2024-11-19 10:50:25 -0500

    Changed default config for network access

--- ESLZ/appServiceWindows.tf
+++ ESLZ/appServiceWindows.tf
@@ -3,7 +3,7 @@ variable "appServiceWindows" {
 }
 
 module "appServiceWindows" {
-  source = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-app_service_windows.git?ref=v1.0.1"
+  source = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-app_service_windows.git?ref=v1.0.2"
   for_each = var.appServiceWindows
 
   userDefinedString = each.key
